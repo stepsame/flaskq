27f4d89177292e1413906c1a0a61950a39276d54
stepsame <stepsame@gmail.com>
2015-12-09 16:22:35 +0800

    auth with flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,16 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.mail import Mail
+from flask.ext.login import LoginManager
 from config import config
 
 bootstrap = Bootstrap()
 moment = Moment()
 db = SQLAlchemy()
 mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -20,6 +24,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     mail.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from . import main
 
 
 @main.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
