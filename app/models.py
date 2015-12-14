ec068ecf14e42a3f90bafb9f754364af1831f88e
stepsame <stepsame@gmail.com>
2015-12-14 16:55:04 +0800

    define default role for users

--- app/models.py
+++ app/models.py
@@ -58,6 +58,14 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
 
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config['FLASKQ_ADMIN']:
+                self.role = Role.query.filter_by(permissions=0xff).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
