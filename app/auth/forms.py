aaa60d9479cb4256f5729f0228a7896e6b429f02
stepsame <stepsame@gmail.com>
2016-01-01 10:03:44 +0800

    design login

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -13,15 +13,15 @@ class LoginForm(Form):
 
 
 class RegistrationForm(Form):
-    email = StringField('Email', validators=[Required(), Length(1, 64),
+    email = StringField('', validators=[Required(), Length(1, 64),
                                              Email()])
-    username = StringField('Username', validators=[
+    username = StringField('', validators=[
         Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                           'Usernames must have only letters,'
                                           'numbers, dots or underscores')])
-    password = PasswordField('Password', validators=[
+    password = PasswordField('', validators=[
         Required(), EqualTo('password2', message='Passwords must match.')])
-    password2 = PasswordField('Confirm password', validators=[Required()])
+    password2 = PasswordField('', validators=[Required()])
     submit = SubmitField('Sign Up')
 
     def validate_email(self, field):
---
+++ app/static/login.js
@@ -0,0 +1,9 @@
+/**
+ * Created by stepsame on 2016/1/1.
+ */
+$("#register-link").click(function (event) {
+    event.preventDefault()
+    $(".form-signup").show()
+    $(".form-signin").hide()
+})
+
