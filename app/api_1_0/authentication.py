906be138daceea71948ff010885f7e53141005df
stepsame <stepsame@gmail.com>
2016-01-01 16:33:13 +0800

    api authentication

---
+++ app/api_1_0/authentication.py
@@ -0,0 +1,32 @@
+from flask import g
+from flask.ext.httpauth import HTTPBasicAuth
+from ..models import AnonymousUser, User
+from . import api
+from .errors import unauthorized, forbidden
+
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(email, password):
+    if email == '':
+        g.current_user = AnonymousUser()
+        return True
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return False
+    g.current_user = user
+    return user.verify_password(password)
+
+
+@auth.error_handler
+def auth_error():
+    return unauthorized('Invalid credentials')
+
+
+@api.before_request
+@auth.login_required
+def before_request():
+    if not g.current_user.is_anonymous and \
+            not g.current_user.confirmed:
+        return forbidden('Unconfirmed account')
